030f6e0e7739c42487933aea6c436b4384346e1d
derk <derk.vandervaart@gmail.com>
2017-03-23 23:03:22 +0100

    X

--- rails/todo_ajax/db/migrate/20170323130717_create_todos.rb
+++ rails/todo_ajax/db/migrate/20170323130717_create_todos.rb
@@ -5,6 +5,21 @@ class CreateTodos < ActiveRecord::Migration[5.0]
       t.boolean :completed, null: false, default: false
 
       t.timestamps
+
+      updateCounters();
+
+        var newTodo = { title: title, completed: false };
+
+        $.ajax({
+          type: "POST",
+          url: "/todos.json",
+          data: JSON.stringify({
+              todo: newTodo
+          }),
+          contentType: "application/json",
+          dataType: "json"
+        });
+      }
     end
   end
 end
